144a5d42d9706dce0aa6606b4e319049c0a85ac3
Piotr Wawrzyniak <hola.wawrzyniak@gmail.com>
2024-02-01 13:18:12 +0100

    Add function initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,10 @@ const select = {
       thisProduct.renderInMenu();
 
       console.log('new Product:', thisProduct);
+
+      thisProduct.initAccordion();
+
+      console.log('Init Accordion:', thisProduct);
     }
     renderInMenu(){
       const thisProduct = this;
@@ -74,7 +78,31 @@ const select = {
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
+    initAccordion(){
+      const thisProduct = this;
+
+     /* find the clickable trigger (the element that should react to clicking) */
+    const clickableTrigger = thisProduct.element.querySelector('.product__header');
+
+    /* START: add event listener to clickable trigger on event click */
+    clickableTrigger.addEventListener('click', function(event) {
+      /* prevent default action for event */
+    event.preventDefault();
+      /* find active product (product that has active class) */
+    const activeProduct = document.querySelector('.product.active');
+      /* if there is active product and it's not thisProduct.element, remove class active from it */
+    if (activeProduct && activeProduct !== thisProduct.element) {
+        activeProduct.classList.remove('active');
+    }
+
+      /* toggle active class on thisProduct.element */
+    thisProduct.element.classList.toggle('active');
+  });
+
   }
+ }
+
+
   const app = {
     initMenu: function() {
       const thisApp = this;
