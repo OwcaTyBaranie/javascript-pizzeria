385dc1ed4c66a81a0b90845a45c8371fe92a223a
Piotr Wawrzyniak <hola.wawrzyniak@gmail.com>
2024-02-20 01:26:16 +0100

    Add new functionality of json. Create second local server/ API

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,11 @@
   },
   };
   const settings = {
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
   amountWidget: {
   defaultValue: 1,
   defaultMin: 1,
@@ -465,13 +470,12 @@
   const thisApp = this;
 
   for (let productData in thisApp.data.products) {
-  new Product(productData, thisApp.data.products[productData]);
+    new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
   }
   },
   init: function () {
   const thisApp = this;
   thisApp.initData();
-  thisApp.initMenu();
   thisApp.initCart();
   },
   initCart: function () {
@@ -484,7 +488,23 @@
   },
   initData: function () {
   const thisApp = this;
-  thisApp.data = dataSource;
+  thisApp.data = {};
+  const url = settings.db.url + '/' + settings.db.products;
+
+  fetch(url)
+  .then(function(rawResponse){
+    return rawResponse.json();
+  })
+  .then(function(parsedResponse){
+    console.log('parsedResponse', parsedResponse);
+
+    /* save parsedResponse as thisApp.data.products */
+    thisApp.data.products = parsedResponse;
+    /* execute initMenu method*/
+    thisApp.initMenu();
+  });
+
+  console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
   };
   app.init();
